a639f58d6701148d60168be521157d64b074eacc
zana-abdollahpour <zana.ap4@gmail.com>
2024-11-21 20:58:47 +0330

    submit user prompt to ai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { runAI } from "@/actions/ai";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   const [aiResponse, setAiResponse] = useState("");
   const [loading, setLoading] = useState(false);
+  const prompt = useRef<string>("");
 
-  const handleRunAI = async () => {
+  const handleRunAI = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setLoading(true);
+
     try {
-      const data = await runAI("write a one line admiration about an artist");
+      const data = await runAI(prompt.current);
       setAiResponse(data);
     } catch (err) {
       if (err instanceof Error) console.error(err.message);
@@ -23,12 +28,23 @@ export default function Home() {
   };
 
   return (
-    <main className="p-4">
-      <Button className="mb-4 p-4" type="button" onClick={handleRunAI}>
-        Ask AI (Google Gemini)
-      </Button>
-      <hr />
-      {loading ? <div>loading...</div> : <p>{aiResponse}</p>}
+    <main className="m-5">
+      <form onSubmit={handleRunAI}>
+        <Input
+          className="mb-5"
+          placeholder="What's on your mind?"
+          onChange={(e) => (prompt.current = e.target.value.trim())}
+        />
+        <Button type="submit">Generate with AI</Button>
+      </form>
+
+      <Card className="mt-5">
+        <CardHeader>AI Response</CardHeader>
+        <CardContent>
+          {/* TODO: add better loading indicator like spinner */}
+          {loading ? <div>loading...</div> : <div>{aiResponse}</div>}
+        </CardContent>
+      </Card>
     </main>
   );
 }
