1f8560add5aa109914c302f7da18b7a94cf6b20c
zana-abdollahpour <zana.ap4@gmail.com>
2024-11-21 20:24:13 +0330

    set up google gemini api

---
+++ src/actions/ai.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey = process.env.GEMINI_API_KEY!;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+export async function runAI() {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage("say a funny dad joke");
+  return result.response.text();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+import { runAI } from "@/actions/ai";
+
 export default function Home() {
+  const [aiResponse, setAiResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleRunAI = async () => {
+    setLoading(true);
+    try {
+      const data = await runAI();
+      setAiResponse(data);
+    } catch (err) {
+      if (err instanceof Error) console.error(err.message);
+      else console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <main>
-      <h1>initial cleanup</h1>
+    <main className="p-4">
+      <button
+        className="bg-red-500 mb-4 p-4"
+        type="button"
+        onClick={handleRunAI}
+      >
+        Ask AI (Google Gemini)
+      </button>
+      <hr />
+      {loading ? <div>loading...</div> : <p>{aiResponse}</p>}
     </main>
   );
 }
