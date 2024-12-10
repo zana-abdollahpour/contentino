cbb7aaff9dc2a1b252c620ee5b89ba96710321a6
zana-abdollahpour <zana.ap4@gmail.com>
2024-12-10 21:59:45 +0330

    improve promo card ui

--- src/components/cards/promo-card.tsx
+++ src/components/cards/promo-card.tsx
@@ -17,12 +17,12 @@ export default function PromoCard({
       <div
         className={twMerge(
           "relative flex h-80 transform flex-col items-start rounded-lg border border-gray-600 p-6 transition-transform",
-          "bg-opacity-80 bg-gradient-to-br from-[#010818] to-[#003366] shadow-lg hover:scale-105 hover:shadow-2xl",
+          "bg-opacity-80 bg-gradient-to-br from-[#2b0044] to-[#c340f0] shadow-lg hover:scale-105 hover:shadow-2xl",
         )}
       >
         <h3 className="mb-2 pt-10 text-lg font-semibold text-white">{title}</h3>
-        <p className="mb-4 text-gray-400">{description}</p>
-        <a href={link} className="text-blue-400 hover:underline">
+        <p className="mb-4 text-gray-300">{description}</p>
+        <a href={link} className="text-blue-500 hover:underline">
           Learn more
         </a>
       </div>
